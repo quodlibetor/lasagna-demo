3ad5d9990832a9cf9d6009ea6ba2e09e75980e0a
Brandon W Maister <bwm@knewton.com>
2018-08-12 16:24:24 -0400

    Update module functions

--- 10-module-functions/src/main.rs
+++ 10-module-functions/src/main.rs
@@ -9,7 +9,7 @@ mod cache {
 
     type CacheMap<K, V> = HashMap<K, V>;
 
-    crate fn get_or_insert<K, V>(
+    pub(crate) fn get_or_insert<K, V>(
         cache: &mut CacheMap<K, V>,
         key: K,
         creator: impl FnOnce(&K) -> V,
@@ -29,11 +29,21 @@ mod cache {
             cache.insert(key.clone(), creator(&key));
         }
     }
+
+}
+
+fn do_something(cache: &mut HashMap<u32, String>, key: u32) {
+    let val = cache::get_or_insert(cache, key, do_something_expensive);
+    // real work with val
 }
 
 fn main() {
-    let mut cachemap = HashMap::new();
+    let mut cache = HashMap::new();
+    do_something(&mut cache, 1_000);
+    do_something(&mut cache, 2);
+    do_something(&mut cache, 1_000);
+}
 
-    let val = cache::get_or_insert(&mut cachemap, "one", |k| k.len());
-    assert_eq!(val, &3);
+fn do_something_expensive(key: &u32) -> String {
+    key.to_string()
 }
